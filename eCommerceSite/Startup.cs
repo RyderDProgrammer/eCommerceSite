ba929f162631bddaccd9af716fa4b06bfb466323
RyderDProgrammer <77074811+RyderDProgrammer@users.noreply.github.com>
2021-07-25 14:19:28 -0700

    Issue#04 create product db (#7)
    * Created Product model.
    
    * Set up DB context.
    
    * Added DbContext to startup. Added EF SQL Server NuGet package
    
    * Added database connection string.
    
    * Added EFCore tools package.
    
    * Added migration to create products table in the database.

--- eCommerceSite/Startup.cs
+++ eCommerceSite/Startup.cs
@@ -1,6 +1,8 @@
+using eCommerceSite.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,7 +26,17 @@ namespace eCommerceSite
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<ProductContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            //Same as up above but instead of a lambda function its the actual function
+            //services.AddDbContext<ProductContext>(AddSqlServer)
         }
+
+        //private void AddSqlServer(DbContextOptionsBuilder options)
+        //{
+        // options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        //}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
