73716b3924642840b256e6c56a7ea58faf12f097
Ryder <rdeback@gmail.com>
2021-07-25 13:40:35 -0700

    Added database connection string.

--- eCommerceSite/Startup.cs
+++ eCommerceSite/Startup.cs
@@ -29,7 +29,14 @@ namespace eCommerceSite
 
             services.AddDbContext<ProductContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            //Same as up above but instead of a lambda function its the actual function
+            //services.AddDbContext<ProductContext>(AddSqlServer)
         }
+
+        //private void AddSqlServer(DbContextOptionsBuilder options)
+        //{
+        // options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        //}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
