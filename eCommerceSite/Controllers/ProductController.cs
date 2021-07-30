7e63c4fda20788cc0c83016ecca9b88439afd1a2
RyderDProgrammer <77074811+RyderDProgrammer@users.noreply.github.com>
2021-07-29 22:06:59 -0700

    Edit product implemented(update in CRUD) (#24)

--- eCommerceSite/Controllers/ProductController.cs
+++ eCommerceSite/Controllers/ProductController.cs
@@ -56,5 +56,35 @@ namespace eCommerceSite.Controllers
             }
             return View();
         }
+        
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            //Get product with corresponding id
+            //Query syntax.
+             Product p =
+              await (from prod in _context.Products
+                    where prod.ProductId == id
+                    select prod).SingleAsync();
+            //Method syntax
+            /*Product p2 =
+                await _context.Products.Where(prod => prod.ProductId == id).SingleAsync();*/
+
+            //pass product to view
+            return View(p);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Product p)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Entry(p).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+
+                ViewData["Message"] = $"{p.Title} updated successfully";
+            }
+            return View(p);
+        }
     }
 }
