cb7934bd0269a1ed0bdd832557cf53beff098faa
Ryder <rdeback@gmail.com>
2021-08-11 19:44:52 -0700

    Single product can now be added to the cart through use of cookies.

--- eCommerceSite/Controllers/CartController.cs
+++ eCommerceSite/Controllers/CartController.cs
@@ -1,20 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace eCommerceSite.Controllers
 {
     public class CartController : Controller
     {
-        public IActionResult Add(int id) // Id of the product to add
+        private readonly ProductContext _context;
+        private readonly IHttpContextAccessor _httpContext;
+        public CartController(ProductContext context, IHttpContextAccessor httpContext)
+        {
+            _context = context;
+            _httpContext = httpContext;
+        }
+        /// <summary>
+        /// Adds a product to the shopping cart
+        /// </summary>
+        /// <param name="id">The id of the product to add</param>
+        /// <returns></returns>
+        public async Task<IActionResult> Add(int id)
         {
             // Get the product from the database
+            Product p = await ProductDB.getProductAsync(_context, id);
+
             // Add the product to cart cookie
+            string data = JsonConvert.SerializeObject(p);
+            CookieOptions options = new CookieOptions()
+            {
+                Expires = DateTime.Now.AddDays(1),
+                Secure = true,
+                IsEssential = true
+            };
+
+            _httpContext.HttpContext.Response.Cookies.Append("CartCookie", data, options);
 
             // Redirect back to previous page
-            return View();
+            return RedirectToAction("Index","Product");
         }
 
         public IActionResult Summary()
